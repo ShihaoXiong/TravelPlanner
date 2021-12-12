b16326a02d9ef3ec8ac960884863d7aa6af9454e
ShihaoXiong <shihao.xiong@icloud.com>
2021-12-11 21:18:41 -0500

    do some configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 import '../style/App.css';
 
 function App() {
---
+++ src/service/index.js
@@ -0,0 +1,46 @@
+/**
+ * @description The encapsulation for axios
+ * @author Shihao Xiong
+ */
+import { message } from 'antd';
+const axios = require('axios');
+
+const METHODS = ['get', 'post', 'put', 'delete'];
+
+// Global config
+const DEFAULT_CONFIG = {
+	timeout: 15 * 1000
+	//  headers: { 'Content-Type': 'application/json' },
+	//  baseURL: BASE_URL
+};
+
+const instance = axios.create(DEFAULT_CONFIG);
+
+/**
+ * Response interecptors
+ */
+instance.interceptors.response.use(
+	res => (res.status >= 200 || res.status < 300 ? res.data : Promise.reject('Request error, please try again later!')),
+	err => {
+		message.error('Request error, please try again later!');
+		return Promise.reject(err);
+	}
+);
+
+const http = METHODS.reduce((pre, key) => {
+	pre[key] = (url, data, func) => {
+		const requestData = {
+			method: key,
+			url,
+			[key === 'get' ? 'params' : 'data']: data
+		};
+
+		func && func(instance);
+
+		return instance.request(requestData);
+	};
+
+	return pre;
+}, {});
+
+export default http;
