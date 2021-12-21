1d29bc836e25e31df5884f4ca279a51529438b41
ShihaoXiong <shihao.xiong@icloud.com>
2021-12-20 21:15:53 -0700

    create google map components

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,17 @@
-import React, { Component, createRef } from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+import React from 'react';
+import { Wrapper } from '@googlemaps/react-wrapper';
 import { API_KEY } from '../service/constant';
-import '../style/Map.css';
+import MapBase from './MapBase';
+import MapMarker from './MapMarker';
 
-const loader = new Loader({
-	apiKey: API_KEY,
-	version: 'weekly'
-});
-
-export default class Map extends Component {
-	constructor() {
-		super();
-		this.mapRef = createRef();
-		this.map = null;
-	}
-
-	componentDidMount() {
-		loader.load().then(google => {
-			this.map = new google.maps.Map(this.mapRef.current, {
-				center: { lat: 40.7127281, lng: -74.0060152 },
-				zoom: 10,
-				mapTypeControl: false,
-				streetViewControl: false,
-				zoomControl: false,
-				fullscreenControl: false
-			});
-		});
-	}
-
-	render() {
-		return <div className='map-container' ref={this.mapRef}></div>;
-	}
+function Map() {
+	return (
+		<Wrapper apiKey={API_KEY}>
+			<MapBase>
+				<MapMarker position={{ lat: 40.7127281, lng: -74.0060152 }} />
+			</MapBase>
+		</Wrapper>
+	);
 }
+
+export default Map;
